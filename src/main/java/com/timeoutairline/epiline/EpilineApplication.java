51e59925469f04ee51296ae9e2f0cb663e8d8fa0
Tristan-Tenders <tristangandi@gmail.com>
2025-12-04 09:56:49 +0100

    first commit

---
+++ src/main/java/com/timeoutairline/epiline/EpilineApplication.java
@@ -0,0 +1,13 @@
+package com.timeoutairline.epiline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpilineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpilineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/timeoutairline/epiline/EpilineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timeoutairline.epiline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpilineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
