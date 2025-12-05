a56d7b97ab7cf52b67c6e7920c4994d81f9bd350
ivan.bomko <vanya.bomko@gmail.com>
2025-12-05 04:48:53 +0100

    client dto

---
+++ src/main/java/com/timeoutairline/epiline/dtos/Client.java
@@ -0,0 +1,17 @@
+package com.timeoutairline.epiline.dtos;
+
+public class Client {
+
+    private Long numPassport;
+
+    public Long getNumPassport(){
+
+        return numPassport;
+    }
+
+    public void setNumPassport(Long numPassport){
+
+        this.numPassport = numPassport;
+    }
+
+}
