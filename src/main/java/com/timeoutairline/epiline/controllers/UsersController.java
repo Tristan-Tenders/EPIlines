9bdf9d8516c2f1e86d1154e3ca2d32c35d9de3e3
Tristan-Tenders <tristangandi@gmail.com>
2025-12-15 15:36:28 +0100

    Update UsersController logic

--- src/main/java/com/timeoutairline/epiline/controllers/UsersController.java
+++ src/main/java/com/timeoutairline/epiline/controllers/UsersController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class UsersController {
     @GetMapping("/users")
     public String Users(){
-        return "Hello";
+        return "HARROW";
     }
 }
